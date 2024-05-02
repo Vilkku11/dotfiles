6455b88299aadaae1398e46752f448a20cf9c2a9
William Reima <william.reima@gmail.com>
2024-05-02 23:41:12 +0300

    fixed kitty opacity script

--- scripts/terminal_opacity.sh
+++ scripts/terminal_opacity.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 CONFIG_PATH=".config/kitty/kitty.conf"
-current_opacity=$(grep "background_opacity" $CONFIG_PATH | awk '{print $2}')
+#current_opacity=$(grep "background_opacity" $CONFIG_PATH | awk 'NR==1{print $2}')
+current_opacity=$(sed -n 's/^background_opacity\s*//p' "$CONFIG_PATH")
+
+#test=$(sed -n 's/^background_opacity\s*//p' "$CONFIG_PATH")
+#echo $current_opacity
+#echo $test
+
 
 if [ "$current_opacity" = "1" ]; then
     new_opacity="0.5"
