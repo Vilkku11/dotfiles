da4ac4793f7290b5d9d77950c83aa57d8f784512
William Reima <william.reima@gmail.com>
2024-06-25 00:05:03 +0300

    qemu + virtmanager install script

---
+++ scripts/vm_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PACKAGES="qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils"
+
+sudo pacman -S --noconfirm --needed $PACKAGES
+
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+
+sudo usermod -aG libvirt $USER
+
+echo "#firewall_backend = "nftables" -> firewall_backend = "iptables" in /etc/libvirt/network.conf"
+sudo sed -i 's/#firewall_backend = "nftables"/firewall_backend = "iptables"/' /etc/libvirt/network.conf
+
+echo "Done! Reboot or log back in!!!"
