83d80d10f8e90c669b94c25a80bc319ef90fc804
William Reima <william.reima@gmail.com>
2024-07-16 01:23:15 +0300

    neovim colorscheme

--- scripts/mirror_update.sh
+++ scripts/mirror_update.sh
@@ -1,3 +1,3 @@
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-sudo reflector --latest 20 --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --latest 20 --age 12 --protocol https --sort rate --verbose --save /etc/pacman.d/mirrorlist
