913dd34cafe893368365e42670d609c22c5f3dcb
William Reima <william.reima@gmail.com>
2025-10-04 17:22:37 +0300

    added scan stuff

--- scripts/install/printer/print_config.sh
+++ scripts/install/printer/print_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES="cups system-config-printer ghostscript python-pyqt5"
+PACKAGES="cups system-config-printer ghostscript python-pyqt5 gscan2pdf djvulibre pdftk"
 
 sudo pacman -S --noconfirm --needed $PACKAGES
 
